ef37cb8762ad3952f9365765b1fe92d46aa04be6
Lukasz Nosko <sneejkuu@gmail.com>
2016-06-29 02:22:58 +0200

    add urls validation

--- notifier/tasks.py
+++ notifier/tasks.py
@@ -1,6 +1,7 @@
 import asyncio
 import logging
 from collections import namedtuple
+from functools import partial
 
 from . import translate, compare, mailer
 
@@ -9,8 +10,20 @@ SECTION_URL = 'https://webtranslateit.com/en/projects/{project_id}-{project_name
 {master_locale}..{other_locale}/strings/{string_id}'
 
 
-Error = namedtuple('Error', ['base', 'other', 'diff', 'section_link', 'file_path', 'base_path', 'other_path'])
+DiffError = namedtuple('DiffError', ['base', 'other', 'diff', 'section_link', 'file_path', 'base_path', 'other_path'])
+UrlError = namedtuple('UrlError', ['url', 'status_code', 'section_link', 'file_path', 'locale'])
 logger = logging.getLogger(__name__)
+
+
+def _make_url_error(base, other, file_path, master_locale, other_locale, section_link, url_diff):
+    locale = master_locale if url_diff.files == base else other_locale
+    return UrlError(
+        url=url_diff.url,
+        status_code=url_diff.status_code,
+        section_link=section_link,
+        file_path=file_path,
+        locale=locale
+    )
 
 
 def filter_filename(files, file_id):
@@ -28,37 +41,48 @@ def compare_with_master(wti_key, mailman_client, string_id, payload, content_typ
     if not project:
         return
     master_locale = project.locales.source
+    other_locale = payload['locale']
     base_string = yield from translate.string(wti_key, master_locale, string_id)
     base = base_string.text
     other = payload['translation']['text']
-    diff = yield from compare.diff(base, other, content_type)
-    if diff:
-        other_locale = payload['locale']
-        filename = filter_filename(project.files, payload['file_id'])
-        error = Error(
-            base=base,
-            other=other,
-            diff=diff[0],
-            section_link=SECTION_URL.format(project_id=project.id, project_name=project.name,
-                                            master_locale=master_locale, other_locale=other_locale,
-                                            string_id=payload['string_id']),
-            file_path='File: {}'.format(filename),
-            base_path='Language: {}'.format(master_locale),
-            other_path='Language: {}'.format(other_locale)
-        )
-        user_id = payload['user_id']
-        user = yield from translate.user(wti_key, user_id)
-        user_email = user.get('email')
-        topic = 'Translations not passing the validation test - file {}, language {}, string_id {}'.format(filename,
-                                                                                                           other_locale,
-                                                                                                           string_id)
+    filename = filter_filename(project.files, payload['file_id'])
+    section_link = SECTION_URL.format(project_id=project.id, project_name=project.name,
+                                      master_locale=master_locale, other_locale=other_locale,
+                                      string_id=payload['string_id']),
+    user_id = payload['user_id']
+    user = yield from translate.user(wti_key, user_id)
+    user_email = user.get('email')
+    topic = 'Translations not passing the validation test - file {}, language {}, string_id {}'.format(filename,
+                                                                                                       other_locale,
+                                                                                                       string_id)
+
+    if content_type == 'ios' and filename[-4:] == '.txt':
+        url_diffs = compare.urls(base, other)
+        url_errors = list(map(partial(_make_url_error, base, other, filename, master_locale, other_locale, section_link),
+                              url_diffs))
         logger.info(topic)
-        if user.get('role') != 'manager':
-            yield from translate.change_status(wti_key, payload['locale'], string_id, other)
-        yield from mailer.send(mailman_client, user_email, [error], content_type, topic)
+        yield from mailer.send(mailman_client, user_email, [], url_errors, content_type, topic)
+    elif filename[-8:] == '.strings':
+        return
     else:
-        # yield from aiohttp.request('PUT', email_cms_host)
-        pass
+        diff = yield from compare.diff(base, other, content_type)
+        if diff:
+            error = DiffError(
+                base=base,
+                other=other,
+                diff=diff[0],
+                section_link=section_link,
+                file_path='File: {}'.format(filename),
+                base_path='Language: {}'.format(master_locale),
+                other_path='Language: {}'.format(other_locale)
+            )
+            logger.info(topic)
+            if user.get('role') != 'manager':
+                yield from translate.change_status(wti_key, payload['locale'], string_id, other)
+            yield from mailer.send(mailman_client, user_email, [error], [], content_type, topic)
+        else:
+            # yield from aiohttp.request('PUT', email_cms_host)
+            pass
 
 
 @asyncio.coroutine
@@ -74,4 +98,4 @@ def validate_project(wti_key, mailman_client, user_email):
             error = yield from compare.diff(base.text, translation.text)
             if error:
                 errors.append(error)
-    yield from mailer.send(mailman_client, user_email, errors)
+    yield from mailer.send(mailman_client, user_email, errors, [])
