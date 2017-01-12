419b04f3f3bdd0b3157c926f29beed0897e31ddf
ca77y <ca77y@live.com>
2017-01-12 19:33:32 +0100

    fix missing const

--- notifier/const.py
+++ notifier/const.py
@@ -1,8 +1,11 @@
 ENCODING = 'utf-8'
+APP_SETTINGS = 'settings'
+
 REQ_APP_KEY = 'wti_apikey'
+REQ_TYPE_KEY = 'type'
+
 EMAIL_PROVIDER = 'srv.email_provider'
 WTI_DYNAMIC_CONTENT = 'srv.wti_dynamic_content'
 ZENDESK_DC = 'srv.zendesk_dynamic_content'
-APP_SETTINGS = 'settings'
 SLACK_NOTIFIER = 'srv.slack'
 STATS = 'srv.stats'
