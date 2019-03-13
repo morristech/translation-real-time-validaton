a24016807616867ab7d905e61b7b6be6016ad5d2
Lukasz Jachym <lukasz.jachym@gmail.com>
2019-03-13 16:05:30 -0700

    adds: test for check_translations call with callback

--- tests/test_validator.py
+++ tests/test_validator.py
@@ -23,3 +23,20 @@ class TestValidator(AsyncTestCase):
         ])
 
         self.coro(validator.check_translations(app, client, WtiContentTypes.md, payload))
+
+    def test_check_translations_callback(self):
+        app = self.make_app()
+        app[const.EMAIL_PROVIDER] = provider = MagicMock()
+        app[const.STATS] = MagicMock()
+        provider.send.return_value = self.make_fut()
+    
+        payload = [read_fixture('payload.json', decoder=json.loads)]
+        client = wti.WtiClient('dummy_api')
+    
+        self.mock_session_new.get.side_effect = iter([
+            AsyncContext(name='session.<request> context', context=self.make_response(read_fixture('project.json'))),
+            AsyncContext(name='session.<request> context', context=self.make_response(read_fixture('translation_en-US.json'))),
+            AsyncContext(name='session.<request> context', context=self.make_response(read_fixture('users.json'))),
+        ])
+        self.coro(validator.check_translations(app, client, WtiContentTypes.md, payload, 'callback-url'))
+        self.mock_session_new.post.assert_called_once()
