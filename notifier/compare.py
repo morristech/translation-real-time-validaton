d9ebf3308b3d6d94916ea17eca7b5b0ae3301e0e
Lukasz Jachym <lukasz.jachym@gmail.com>
2019-05-31 14:01:30 +0200

    hotfix: adjust validator to plural WtiStrings

--- notifier/compare.py
+++ notifier/compare.py
@@ -7,19 +7,30 @@ from . import const
 from .model import *
 
 
+def get_text(wti_string):
+    if not wti_string.plural:
+        return wti_string.text
+    text = ''
+    for _, text in wti_string.text.items():
+        text += '\n\n%s\n\n' % text
+    return text
+
+
 def diff(wti_client, project, base, other):
     _, filename_ext = os.path.splitext(project.filename)
     section_link = wti_client.section_link(project, other)
+    base_text = get_text(base)
+    other_text = get_text(other)
     if filename_ext == '.strings':
         return None
     elif project.content_type == WtiContentTypes.ios and filename_ext == '.txt':
-        return DiffError(_urls(base.text, other.text), None, section_link)
+        return DiffError(_urls(base_text, other_text), None, section_link)
     elif project.content_type == WtiContentTypes.java:
-        return DiffError(None, _java_diff(base.text, other.text), section_link)
+        return DiffError(None, _java_diff(base_text, other_text), section_link)
     else:
-        md_errors = _md_diff(base.text, other.text)
+        md_errors = _md_diff(base_text, other_text)
         md_error = md_errors[0] if len(md_errors) > 0 else None
-        return DiffError(_urls(base.text, other.text), md_error, section_link)
+        return DiffError(_urls(base_text, other_text), md_error, section_link)
 
 
 def _urls(base, other):
--- tests/test_compare.py
+++ tests/test_compare.py
@@ -19,6 +19,36 @@ class TestCompare(AsyncTestCase):
         compare.diff(self.client, project, base, other)
         self.assertFalse(self.mock_request.called)
 
+    def test_valid_plural_strings(self):
+        text_base = {
+            'one': 'AAA',
+            'two': 'BBB'
+        }
+        text_other = {
+            'one': 'ABB',
+            'two': 'CAA'
+        }
+        project = WtiProject('dummy_id', 'dummy_name', 'dummy_locale', 'file.md', WtiContentTypes.md)
+        base = WtiString('id1', 'locale1', text_base, 'STATUS', '2015-01-22T00:05:01Z', True)
+        other = WtiString('id2', 'locale2', text_other, 'STATUS', '2015-01-22T00:05:01Z', True)
+        diff = compare.diff(self.client, project, base, other)
+        self.assertFalse(diff.md_error)
+
+    def test_invalid_plural_strings(self):
+        text_base = {
+            'one': 'AAA',
+            'two': 'BBB'
+        }
+        text_other = {
+            'one': 'ABB',
+            'two': '# CAA'
+        }
+        project = WtiProject('dummy_id', 'dummy_name', 'dummy_locale', 'file.md', WtiContentTypes.md)
+        base = WtiString('id1', 'locale1', text_base, 'STATUS', '2015-01-22T00:05:01Z', True)
+        other = WtiString('id2', 'locale2', text_other, 'STATUS', '2015-01-22T00:05:01Z', True)
+        diff = compare.diff(self.client, project, base, other)
+        self.assertTrue(diff.md_error)
+
     def _test_ios_text(self):
         project = WtiProject('dummy_id', 'dummy_name', 'dummy_locale', 'file.txt', WtiContentTypes.ios)
         base = WtiString('id1', 'locale1', 'aaa http://www.link.com aaa', 'STATUS', '2015-01-22T00:05:01Z', False)
